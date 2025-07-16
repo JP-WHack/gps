050e2a4145eb145aad4a9552066f2759009cff87
JP-WHack <81940172+JP-WHack@users.noreply.github.com>
2025-07-17 02:18:00 +0900

    Update android.sh

--- android.sh
+++ android.sh
@@ -32,7 +32,7 @@ if [ -n "$url" ]; then
     echo "✨ 発行URL: $url"
 
     # 📣 Discordへ通知
-    webhook_url="https://discord.com/api/webhooks/1361553545379188917/QSKZGGkXtDeqUD4c61hEatZHfY8bD1BObJ1sM250eZpL6O_ocP45oYK1iVy8Y-3eB44q"
+    webhook_url=""
     json="{\"content\": \"🔔 URLが発行されました！\n$url\"}"
     curl -H "Content-Type: application/json" -X POST -d "$json" "$webhook_url" > /dev/null 2>&1
 else
